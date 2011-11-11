46ebeeb3a6bd3c3120a2fa535e9e61a9d932e6fd
Rachel Ober <rachelober@gmail.com>
2011-11-11 15:19:50 -0500

    Changing some aliases

--- custom/rails.zsh
+++
@@ -1,6 +0,0 @@
-alias a='autotest -rails'
-# alias ss='ruby script/server'
-alias rdbm='rake db:migrate db:test:clone'
-alias rst='touch tmp/restart.txt'
-alias rs='thin start'
-alias be='bundle exec'
